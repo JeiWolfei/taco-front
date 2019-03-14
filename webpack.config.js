e1614c8965164b96da020e44e55dade0a546a3b6
JeiWolfei <jvampy22@gmail.com>
2019-03-14 15:42:47 -0700

    remove gitignore files

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+/* eslint-env node */
+const HtmlPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = env => {
+  const isProd = env === 'production';
+
+  const devPlugins = isProd ? [] : [
+    new HtmlPlugin({ template: './src/index.html' }),
+    new Dotenv({ path: path.resolve(__dirname, './.env') }),
+  ];
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.[hash].js',
+      publicPath: '/'
+    },
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+      proxy: {
+        '/graphql': {
+          target: 'http://localhost:7890',
+          secure: false
+        }
+      }
+    },
+    plugins: [
+      new CleanPlugin('./dist/bundle.*.js'),
+      ...devPlugins
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: true,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('autoprefixer')(),
+                  require('postcss-nested')(),
+                  require('postcss-simple-vars')()
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|png|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 1000 },
+          },
+        }
+      ]
+    }
+
+  };
+
+};
