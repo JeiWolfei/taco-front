37c3fa84f80a31042ccc51a19db235e1462f803e
JeiWolfei <jvampy22@gmail.com>
2019-03-18 20:20:52 -0700

    added TacoStat

---
+++ src/containers/stat/TacoStat.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { fetchStat } from '../../actions/tacos';
+import { getTacoImage, getTacoName, getStat, isLoading } from '../../selectors/tacos';
+import Stat from '../../components/stat/Stat';
+import { withFetch } from '../../components/stat/withFetch';
+
+const mapStateToProps = state => ({
+  stat: getStat(state),
+  tacoName: getTacoName(state),
+  tacoImage: getTacoImage(state),
+  loading: isLoading(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    return dispatch(fetchStat());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withFetch(Stat));
